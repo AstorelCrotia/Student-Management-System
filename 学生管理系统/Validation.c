f8aaa20524b3716f55cc2730a495db4b6017158a
Astorel <163396646+AstorelCrotia@users.noreply.github.com>
2024-03-16 14:15:41 +0800

    commit

--- 学生管理系统/Validation.c
+++ 学生管理系统/Validation.c
@@ -1,9 +1,10 @@
 #include "head.h"
 
-void Validation(char username) //验证功能
+void Validation(char * username) //验证功能
 {
     int tureusername = 0;
     int flag = 0;
+    int i = 1;
     FILE* file_read = fopen("users.txt","r");
     if(file_read == NULL)
     {
@@ -11,23 +12,30 @@ void Validation(char username) //验证功能
         return;
     }
     fread(tureusername,sizeof(Student),1,file_read);
-
-    strcmp(username,tureusername);
-
-    while(username == tureusername)
+    while(i != 3)
     {
-        printf("用户名不存在！\n你可以输入“1”重新输入，输入“2”退出：");
-        scanf("%d",flag);
-        if (flag == 2)
+        if(strcmp(username,tureusername) == 0)
         {
+            printf("欢迎登录！\n");
             break;
         }
         else
         {
-            printf("请输入用户名:");
-            scanf("%s",username);
+            printf("用户名不存在！\n");
+            printf("这是第%d次输入错误，你还有%d次机会\n",i,3-i);
+            printf("你可以输入“1”重新输入，输入“2”退出：");
+            scanf("%d",flag);
+            if (flag == 2)
+            {
+                break;
+            }
+            else
+            {
+                printf("请输入用户名:");
+                scanf("%s",username);
+            }
         }
+        i++;
     }
-    printf("用户存在");
     fclose(file_read);
 }
